e78e39a7575d3d93b7c23da51811039f2e157922
John Evans <lgastako@gmail.com>
2013-03-29 22:15:24 -0700

    A little cleanup.

--- Tests.hs
+++ Tests.hs
@@ -1,16 +1,14 @@
 module Main where
 
 import Test.QuickCheck
-import Test.Framework (defaultMain, testGroup)
+import Test.Framework (defaultMain)
 import Test.Framework (Test)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 
 import Aldus.Core(mkrel,destPath)
-import Data.String.Utils(join)
 
 
---slashes :: String
-
+slashes :: String
 slashes = '/' : slashes
 
 -- ** begin tests ** --
@@ -24,18 +22,22 @@ prop_mkrel_drops_prefix s = length s > 0 && (head s) /= '/' && (head s) /= '.' =
 
 -- ** destPath ** --
 
+prop_destPath_moves :: String -> Bool
 prop_destPath_moves s = destPath "/src" "/dst" ("/src/" ++ s) == "/dst/" ++ s
 
 -- destPath should normalize any repeated slashes to a single slash, so for any
 --     destPath "/src" "/dst" "/src////" + (whatever) the result should b "/dst/" + (whatever)
 --     except in the case of "/src" or equivalent which should result in "/dst"
 
+prop_destPath_drops_double_slash :: String -> Property
 prop_destPath_drops_double_slash s =
     (s /= "") ==>
         destPath "/src" "/dst" ("/src////" ++ s) == "/dst/" ++ s
 
 -- No matter how many slashes there are on the end of the exact srcDir
-prop_destPath_handles_src_equiv n = destPath "/src" "/dst" ("/src" ++ s)
+--
+prop_destPath_handles_src_equiv :: Int -> Bool
+prop_destPath_handles_src_equiv n = destPath "/src" "/dst" ("/src" ++ s) == "/dst"
     where s = take n slashes
 
 prop_destPath_drops_trailing_slash s = destPath "/src" "/dst" ("/src/" ++ s ++ "/") == "/dst/" ++ s
